01ad321a8fa2b540093cb993fe13fec4cb117887
stutxo <70952638+stutxo@users.noreply.github.com>
2025-05-13 09:31:20 -0400

    clean up code and allow ipv6 libre relay nodes

--- src/main.rs
+++ src/main.rs
@@ -38,15 +38,14 @@ async fn main() -> Result<()> {
 
     info!("Time to blast some nodes with pigeon poop! 🕊️💩");
     let args = Args::parse();
-    let tx_string = args.tx;
-    let tx = bitcoin::consensus::deserialize::<Transaction>(&hex::decode(tx_string.clone())?)?;
-    let mut tx_bytes = Vec::new();
+    let tx_hex_string = args.tx;
+    let tx = bitcoin::consensus::deserialize::<Transaction>(&hex::decode(tx_hex_string)?)?;
+    let mut tx_raw_msg_bytes = Vec::new();
     RawNetworkMessage::new(Magic::BITCOIN, NetworkMessage::Tx(tx.clone()))
-        .consensus_encode(&mut tx_bytes)?;
+        .consensus_encode(&mut tx_raw_msg_bytes)?;
 
     let mut seed_addrs = Vec::new();
-
-    let seeds: Vec<&'static str> = vec![
+    let dns_seeds: Vec<&'static str> = vec![
         "dnsseed.bluematt.me",
         "dnsseed.bitcoin.dashjr.org",
         "seed.bitcoinstats.com",
@@ -56,53 +55,48 @@ async fn main() -> Result<()> {
         "seed.bitcoin.wiz.biz",
     ];
 
-    for seed in seeds {
-        info!("fetching addrs from {:?}", seed);
-
-        if let Ok(addrs) = lookup_host(format!("{}:8333", seed)).await {
-            seed_addrs.extend(addrs.into_iter());
+    for seed_host in dns_seeds {
+        info!("fetching addrs from {:?}", seed_host);
+        if let Ok(resolved_addrs) = lookup_host(format!("{}:8333", seed_host)).await {
+            seed_addrs.extend(resolved_addrs);
         }
     }
 
-    info!("found {} seeds", seed_addrs.len());
+    info!("found {} potential seed node addresses", seed_addrs.len());
 
-    let mut libre = HashSet::<SocketAddr>::new();
-    let mut tasks = FuturesUnordered::new();
-    for a in seed_addrs {
-        tasks.push(tokio::spawn(async move { crawl_seed(a).await }));
+    let mut libre_peers = HashSet::<SocketAddr>::new();
+    let mut crawl_tasks = FuturesUnordered::new();
+    for addr in seed_addrs {
+        crawl_tasks.push(tokio::spawn(crawl_seed_node(addr)));
     }
-    while let Some(Ok(list)) = tasks.next().await {
-        if let Ok(addresses) = list {
-            libre.extend(addresses);
-        }
+    while let Some(Ok(Ok(addresses))) = crawl_tasks.next().await {
+        libre_peers.extend(addresses);
     }
-    let peers: Vec<_> = libre.into_iter().filter(|a| a.is_ipv4()).collect();
 
-    let mut poops = FuturesUnordered::new();
-    for p in peers.clone() {
-        let txb = tx_bytes.clone();
-        poops.push(tokio::spawn(async move {
-            let _ = poop_tx(p, &txb).await;
-            p
+    let mut poop_delivery_tasks = FuturesUnordered::new();
+    for peer_addr in libre_peers.clone() {
+        let tx_bytes_clone = tx_raw_msg_bytes.clone();
+        poop_delivery_tasks.push(tokio::spawn(async move {
+            let _ = deliver_poop_tx(peer_addr, &tx_bytes_clone).await;
+            peer_addr
         }));
     }
 
-    info!("pooping on {} libre nodes", peers.len());
-
-    while let Some(Ok(addr)) = poops.next().await {
-        info!("you pooped on libre node {addr}");
+    info!("pooping on {} libre nodes", libre_peers.len());
+    while let Some(Ok(addr)) = poop_delivery_tasks.next().await {
+        info!("you pooped on libre node {}", addr);
     }
 
     info!(
-        "tx {:?} blasted to {} libre nodes. GLHF",
+        "TX: {:?} blasted to {} libre relay nodes. GLHF",
         tx.compute_txid(),
-        peers.len(),
+        libre_peers.len(),
     );
 
     Ok(())
 }
 
-fn build_version(addr: SocketAddr) -> VersionMessage {
+fn build_version_msg(addr: SocketAddr) -> VersionMessage {
     VersionMessage {
         version: 70016,
         services: ServiceFlags::from(NODE_LIBRE_RELAY),
@@ -122,6 +116,87 @@ fn build_version(addr: SocketAddr) -> VersionMessage {
     }
 }
 
+async fn deliver_poop_tx(addr: SocketAddr, tx_bytes: &[u8]) -> Result<()> {
+    let mut stream =
+        tokio::time::timeout(Duration::from_millis(500), TcpStream::connect(addr)).await??;
+    stream.set_nodelay(true)?;
+
+    send_msg(
+        &mut stream,
+        NetworkMessage::Version(build_version_msg(addr)),
+    )
+    .await?;
+
+    let (mut rd, mut wr) = stream.split();
+    tokio::time::timeout(Duration::from_millis(1000), async {
+        loop {
+            if let NetworkMessage::Version(_) = read_msg(&mut rd).await?.payload() {
+                break Ok::<(), anyhow::Error>(());
+            }
+        }
+    })
+    .await??;
+
+    send_msg(&mut wr, NetworkMessage::Verack).await?;
+    wr.write_all(tx_bytes).await?;
+    wr.flush().await?;
+    Ok(())
+}
+
+async fn crawl_seed_node(seed: SocketAddr) -> Result<Vec<SocketAddr>> {
+    let mut found_peers = Vec::new();
+    info!("crawling seed {:?}", seed);
+    let mut stream =
+        match tokio::time::timeout(Duration::from_millis(500), TcpStream::connect(seed)).await {
+            Ok(Ok(s)) => s,
+            _ => return Ok(found_peers),
+        };
+    stream.set_nodelay(true)?;
+
+    send_msg(
+        &mut stream,
+        NetworkMessage::Version(build_version_msg(seed)),
+    )
+    .await?;
+
+    let (mut rd, mut wr) = stream.split();
+
+    info!("waiting for addresses from {:?}...", seed);
+    loop {
+        let msg = match tokio::time::timeout(Duration::from_millis(1000), read_msg(&mut rd)).await {
+            Ok(Ok(m)) => m,
+            _ => break,
+        };
+        match msg.payload() {
+            NetworkMessage::Version(_) => {
+                send_msg(&mut wr, NetworkMessage::Verack).await?;
+                send_msg(&mut wr, NetworkMessage::GetAddr).await?;
+            }
+            NetworkMessage::Addr(list) => {
+                let flag = ServiceFlags::from(NODE_LIBRE_RELAY);
+                found_peers.extend(list.iter().filter_map(|(_, a)| {
+                    if a.services.has(flag) {
+                        a.socket_addr().ok()
+                    } else {
+                        None
+                    }
+                }));
+                break;
+            }
+            _ => {}
+        }
+    }
+    Ok(found_peers)
+}
+
+async fn send_msg<S: AsyncWriteExt + Unpin>(stream: &mut S, msg: NetworkMessage) -> Result<()> {
+    let mut buf = Vec::new();
+    RawNetworkMessage::new(Magic::BITCOIN, msg).consensus_encode(&mut buf)?;
+    stream.write_all(&buf).await?;
+    stream.flush().await?;
+    Ok(())
+}
+
 async fn read_msg<R: AsyncReadExt + Unpin>(r: &mut R) -> Result<RawNetworkMessage> {
     let mut hdr = [0u8; 24];
     r.read_exact(&mut hdr).await?;
@@ -131,89 +206,4 @@ async fn read_msg<R: AsyncReadExt + Unpin>(r: &mut R) -> Result<RawNetworkMessag
     Ok(RawNetworkMessage::consensus_decode(&mut Cursor::new(
         [hdr.to_vec(), payload].concat(),
     ))?)
-}
-
-async fn poop_tx(addr: SocketAddr, tx: &[u8]) -> Result<()> {
-    let mut stream =
-        tokio::time::timeout(Duration::from_millis(800), TcpStream::connect(addr)).await??;
-    stream.set_nodelay(true)?;
-
-    // send VERSION
-    let mut buf = Vec::new();
-    RawNetworkMessage::new(Magic::BITCOIN, NetworkMessage::Version(build_version(addr)))
-        .consensus_encode(&mut buf)?;
-    stream.write_all(&buf).await?;
-    stream.flush().await?;
-
-    let (mut rd, mut wr) = stream.split();
-    // wait for their VERSION
-    tokio::time::timeout(Duration::from_millis(800), async {
-        loop {
-            if let NetworkMessage::Version(_) = read_msg(&mut rd).await?.payload() {
-                break Ok::<(), anyhow::Error>(());
-            }
-        }
-    })
-    .await??;
-
-    // VERACK + TX
-    buf.clear();
-    RawNetworkMessage::new(Magic::BITCOIN, NetworkMessage::Verack).consensus_encode(&mut buf)?;
-    wr.write_all(&buf).await?;
-    wr.write_all(tx).await?;
-    wr.flush().await?;
-    Ok(())
-}
-
-async fn crawl_seed(seed: SocketAddr) -> Result<Vec<SocketAddr>> {
-    let mut peers = Vec::new();
-    info!("crawling seed {:?}", seed);
-    let mut stream =
-        match tokio::time::timeout(Duration::from_millis(800), TcpStream::connect(seed)).await {
-            Ok(Ok(s)) => s,
-            _ => return Ok(peers),
-        };
-    stream.set_nodelay(true)?;
-
-    // send VERSION
-    let mut buf = Vec::new();
-    RawNetworkMessage::new(Magic::BITCOIN, NetworkMessage::Version(build_version(seed)))
-        .consensus_encode(&mut buf)?;
-    stream.write_all(&buf).await?;
-    stream.flush().await?;
-
-    let (mut rd, mut wr) = stream.split();
-
-    loop {
-        let msg = match tokio::time::timeout(Duration::from_millis(800), read_msg(&mut rd)).await {
-            Ok(Ok(m)) => m,
-            _ => break,
-        };
-        match *msg.payload() {
-            NetworkMessage::Version(_) => {
-                buf.clear();
-                RawNetworkMessage::new(Magic::BITCOIN, NetworkMessage::Verack)
-                    .consensus_encode(&mut buf)?;
-                wr.write_all(&buf).await?;
-                buf.clear();
-                RawNetworkMessage::new(Magic::BITCOIN, NetworkMessage::GetAddr)
-                    .consensus_encode(&mut buf)?;
-                wr.write_all(&buf).await?;
-                wr.flush().await?;
-            }
-            NetworkMessage::Addr(ref list) => {
-                let flag = ServiceFlags::from(NODE_LIBRE_RELAY);
-                for (_, a) in list {
-                    if a.services.has(flag) {
-                        if let Ok(sa) = a.socket_addr() {
-                            peers.push(sa);
-                        }
-                    }
-                }
-                break;
-            }
-            _ => {}
-        }
-    }
-    Ok(peers)
 }
